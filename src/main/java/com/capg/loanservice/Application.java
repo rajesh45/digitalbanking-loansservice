a7bfad8f4f054e398f2b265cf79bf0b4c9f59db6
Mahesh Natarajan <umamags@gmail.com>
2017-05-25 14:16:13 +0530

    war runnable

--- src/main/java/com/capg/loanservice/Application.java
+++ src/main/java/com/capg/loanservice/Application.java
@@ -3,6 +3,7 @@ package com.capg.loanservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 //import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,16 +11,20 @@ import org.apache.log4j.Logger;
 
 @SpringBootApplication
 @ComponentScan("com.capg.loanservice")
-public class Application   {
-	
-    private static Logger log = Logger.getLogger(Application.class);
+public class Application extends SpringBootServletInitializer {
 
+	private static Logger log = Logger.getLogger(Application.class);
 
-    public static void main(final String[] args) throws Throwable {
-    	
-        log.info("Logger enabled: Entering main \n\n");
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(final String[] args) throws Throwable {
+
+		log.info("Logger enabled: Entering main \n\n");
+
+		SpringApplication.run(Application.class, args);
+	}
 
 }
