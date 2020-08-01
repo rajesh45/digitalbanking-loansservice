018ef8e09684e0102c54b170badfe2b21a7c8835
Rajesh Yadav <rajesh.seacom@gmail.com>
2020-08-01 15:05:39 +0530

    commit my changes

--- src/main/java/com/capg/loanservice/model/Transaction.java
+++ src/main/java/com/capg/loanservice/model/Transaction.java
@@ -19,7 +19,7 @@ public class Transaction implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="emi_no")
 	private int emiNo; 
 	
